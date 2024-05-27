a8293f17d6c595fbcc002c93a7e3e0bd4776b9cc
jean.333 <41438361+Yoojin99@users.noreply.github.com>
2024-05-27 21:59:53 +0900

    Create high order reducer
    #5

--- SUPlayground/ContentView.swift
+++ SUPlayground/ContentView.swift
@@ -77,21 +77,6 @@ struct AppState: Codable  {
             case removedFavoritePrime(Int)
         }
     }
-    
-    // keypath 생성용
-    var favoritePrimesState: FavoritePrimesState {
-        get {
-            FavoritePrimesState(
-                favoritePrimes: self.favoritePrimes, 
-                activityFeed: self.activityFeed
-            )
-        }
-        
-        set {
-            self.favoritePrimes = newValue.favoritePrimes
-            self.activityFeed = newValue.activityFeed
-        }
-    }
 }
 
 /*
@@ -172,27 +157,19 @@ func primeModalReducer(state: inout AppState, action: PrimeModalAction) {
     switch action {
     case .saveFavoritePrimeTapped:
         state.favoritePrimes.insert(state.count)
-        state.activityFeed.append(.init(timestamp: Date(), type: .addedFavoritePrime(state.count)))
     case .removeFavoritePrimeTapped:
         state.favoritePrimes.remove(state.count)
-        state.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(state.count)))
     }
 }
 
-struct FavoritePrimesState {
-    var favoritePrimes: Set<Int>
-    var activityFeed: [AppState.Activity]
-}
-
-func favoritePrimesReducer(state: inout FavoritePrimesState, action: FavoritePrimesAction) {
+func favoritePrimesReducer(state: inout Set<Int>, action: FavoritePrimesAction) {
     switch action {
     case .deleteFavoritePrimes(let indexSet):
-        let favoritePrimes = state.favoritePrimes.sorted()
+        let favoritePrimes = state.sorted()
         let removedNumbers = indexSet.map{ favoritePrimes[$0] }
         
         for number in removedNumbers {
-            state.favoritePrimes.remove(number)
-            state.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(number)))
+            state.remove(number)
         }
     }
 }
@@ -247,10 +224,32 @@ struct EnumKeyPath<Root, Value> {
 
 // \AppAction.counter // EnumKeyPath<AppAction, CounterAction>
 
+func activityFeed(
+    _ reducer: @escaping (inout AppState, AppAction) -> Void
+) -> (inout AppState, AppAction) -> Void {
+    return { state, action in
+        switch action {
+            
+        case .counter:
+            break
+        case .primeModal(.removeFavoritePrimeTapped):
+            state.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(state.count)))
+        case .primeModal(.saveFavoritePrimeTapped):
+            state.activityFeed.append(.init(timestamp: Date(), type: .addedFavoritePrime(state.count)))
+        case let .favoritePrimes(.deleteFavoritePrimes(removedNumbers)):
+            for number in removedNumbers {
+                state.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(number)))
+            }
+        }
+        
+        reducer(&state, action)
+    }
+}
+
 let appReducer: (inout AppState, AppAction) -> Void = combine(
     pullback(counterReducer, value: \.count, action: \.counter),
     pullback(primeModalReducer, value: \.self, action: \.primeModal),
-    pullback(favoritePrimesReducer, value: \.favoritePrimesState, action: \.favoritePrimes)
+    pullback(favoritePrimesReducer, value: \.favoritePrimes, action: \.favoritePrimes)
 )
 
 
@@ -457,5 +456,5 @@ struct FavoritePrimesView: View {
 }
 
 #Preview {
-    ContentView(store: Store<AppState, AppAction>(initialValue: AppState(), reducer: appReducer))
+    ContentView(store: Store<AppState, AppAction>(initialValue: AppState(), reducer: activityFeed(appReducer)))
 }
